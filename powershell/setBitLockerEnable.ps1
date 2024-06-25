58e12d2b0b55fc10fae01eb7a76162274a45187b
Atelier de Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2024-06-25 23:06:25 +0200

    Update setBitLockerEnable.ps1

--- powershell/setBitLockerEnable.ps1
+++ powershell/setBitLockerEnable.ps1
@@ -1,1 +1,3 @@
-#
+$systemDrive = (Get-BitLockerVolume | Where {$_.VolumeType -eq "OperatingSystem"}).MountPoint 
+Write-Output "Volumen de sistema: $systemDrive"
+Enable-BitLocker -MountPoint "$systemDrive" -RecoveryPasswordProtector
