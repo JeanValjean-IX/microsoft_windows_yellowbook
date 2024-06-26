2cd4e15839ef3a1eba7c40f02272226ee772cb39
Atelier de Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2024-06-26 19:38:29 +0200

    Update setBitLockerDisableSystemDrive.ps1

--- powershell/setBitLockerDisableSystemDrive.ps1
+++ powershell/setBitLockerDisableSystemDrive.ps1
@@ -1,2 +1,24 @@
-
-Disable-BitLocker -MountPoint
+$systemDrive = (Get-BitLockerVolume | Where {$_.VolumeType -eq "OperatingSystem"}).MountPoint
+Write-Output "Volumen de sistema: $systemDrive"
+$BitLockerStatus = Get-BitLockerVolume -MountPoint $systemDrive
+Disable-BitLocker -MountPoint "$systemDrive"
+$BitLockerStatus = Get-BitLockerVolume -MountPoint $systemDrive
+if ($BitLockerStatus.ProtectionStatus -eq 'On') {
+    Write-Host "Iniciando desencriptación..."
+    Unlock-BitLocker -MountPoint $SystemDrive
+} else {
+    Write-Host "BitLocker deshabilitado o unidad de sistema no cifrada."
+}
+$DecryptionStatus = Get-BitLockerVolume -MountPoint $systemDrive
+if ($DecryptionStatus.VolumeStatus -eq 'FullyDecrypted') {
+    Write-Host "La unidad del sistema ya está completamente desencriptada."
+} else {
+    Write-Host "Esperando a que la desencriptación finalice. Esto puede tardar algún tiempo..."
+    while ($DecryptionStatus.VolumeStatus -ne 'FullyDecrypted') {
+        Start-Sleep -Seconds 60
+        $DecryptionStatus = Get-BitLockerVolume -MountPoint $systemDrive
+        Write-Host "Progreso -> $($DecryptionStatus.EncryptionPercentage)%"
+    }
+    Write-Host "Desencriptación completada."
+}
+Write-Host "BitLocker deshabilitado y unidad de sistema desencriptada."
