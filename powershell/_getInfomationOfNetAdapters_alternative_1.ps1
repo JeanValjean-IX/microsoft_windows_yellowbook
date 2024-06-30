535c29317af2b429d7d8f16e7b6bac874de02224
Atelier de Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2024-06-30 23:14:51 +0200

    Update _getInfomationOfNetAdapters_alternative_1.ps1

--- powershell/_getInfomationOfNetAdapters_alternative_1.ps1
+++ powershell/_getInfomationOfNetAdapters_alternative_1.ps1
@@ -1,6 +1,5 @@
-# Get-NetAdapter | Select-Object Name, InterfaceDescription, Status, VlanID
-
 # Obtener información detallada de las targetas de red del equipo
+
 $networkAdapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration
 
 foreach ($adapter in $networkAdapters) {
@@ -14,17 +13,3 @@ foreach ($adapter in $networkAdapters) {
     Write-Host "DNS Servers: $($adapter.DNSServerSearchOrder)"
     Write-Host "VLAN ID: $($adapter.SettingID)" # Este campo puede no reflejar la VLAN directamente
 }
-
-# Alternativa: Usar NetAdapter para obtener información detallada del adaptador si está disponible
-$netAdapters = Get-NetAdapter
-
-foreach ($adapter in $netAdapters) {
-    Write-Host "---------------------------------"
-    Write-Host "Name: $($adapter.Name)"
-    Write-Host "Interface Description: $($adapter.InterfaceDescription)"
-    Write-Host "Status: $($adapter.Status)"
-    Write-Host "Link Speed: $($adapter.LinkSpeed)"
-    Write-Host "Mac Address: $($adapter.MacAddress)"
-    Write-Host "VLAN ID: $($adapter.VlanID)" # Este campo puede no estar disponible en todos los adaptadores
-}
-
