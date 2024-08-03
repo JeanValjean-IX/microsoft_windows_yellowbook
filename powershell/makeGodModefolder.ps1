88e0f431f569d3435e07f09276413c57f1bb2195
Atelier de Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2024-08-03 23:59:11 +0200

    Update makeGodModefolder.ps1

--- powershell/makeGodModefolder.ps1
+++ powershell/makeGodModefolder.ps1
@@ -1,7 +1,10 @@
-$folder = "GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
-if (-Not (Test-Path -Path $folder)) {
-    # Create the GodMode folder
-    New-Item -Path $folder -ItemType Directory
+param (
+    [string]$folderPath = ""
+)
+$GodModefolder = "GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
+$fullFolderPath = Join-Path -Path $folderPath -ChildPath $GodModefolder
+if (-Not (Test-Path -Path $GodModefolder)) {
+    New-Item -Path $fullFolderPath -ItemType Directory
     Write-Host "GodMode folder created"
 } else {
     Write-Host "GodMode folder already exists"
