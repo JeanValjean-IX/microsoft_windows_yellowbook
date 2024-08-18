3a642b1afe2aa6763000106082f55fc8944d7875
Atelier de Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2024-08-18 21:30:17 +0200

    Update getInfomationOfNetAdapters_alternative2.ps1

--- powershell/getInfomationOfNetAdapters_alternative2.ps1
+++ powershell/getInfomationOfNetAdapters_alternative2.ps1
@@ -6,10 +6,10 @@ $netAdapters = Get-NetAdapter
 
 foreach ($adapter in $netAdapters) {
     Write-Host "---------------------------------"
-    Write-Host "Name: $($adapter.Name)"
-    Write-Host "Interface Description: $($adapter.InterfaceDescription)"
-    Write-Host "Status: $($adapter.Status)"
-    Write-Host "Link Speed: $($adapter.LinkSpeed)"
-    Write-Host "Mac Address: $($adapter.MacAddress)"
-    Write-Host "VLAN ID: $($adapter.VlanID)" # Este campo puede no estar disponible en todos los adaptadores
+    Write-Host "Name        : $($adapter.Name)"
+    Write-Host "Description : $($adapter.InterfaceDescription)"
+    Write-Host "Status      : $($adapter.Status)"
+    Write-Host "Link Speed  : $($adapter.LinkSpeed)"
+    Write-Host "Mac Address : $($adapter.MacAddress)"
+    Write-Host "VLAN ID     : $($adapter.VlanID)"
 }
