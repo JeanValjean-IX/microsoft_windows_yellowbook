998ddd1816a622891bbd20c03e6c8a973688352b
Atelier de Valjean <111537317+JeanValjean-IX@users.noreply.github.com>
2024-08-18 21:25:00 +0200

    Update getInfomationOfNetAdapters_alternative1.ps1

--- powershell/getInfomationOfNetAdapters_alternative1.ps1
+++ powershell/getInfomationOfNetAdapters_alternative1.ps1
@@ -4,12 +4,12 @@ $networkAdapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration
 
 foreach ($adapter in $networkAdapters) {
     Write-Host "---------------------------------"
-    Write-Host "Nombre: $($adapter.Description)"
-    Write-Host "MAC Address: $($adapter.MACAddress)"
-    Write-Host "DHCP Habilitado: $($adapter.DHCPEnabled)"
-    Write-Host "IP Address: $($adapter.IPAddress)"
-    Write-Host "Subnet Mask: $($adapter.IPSubnet)"
-    Write-Host "Default Gateway: $($adapter.DefaultIPGateway)"
-    Write-Host "DNS Servers: $($adapter.DNSServerSearchOrder)"
-    Write-Host "VLAN ID: $($adapter.SettingID)" # Este campo puede no reflejar la VLAN directamente
+    Write-Host "Nombre          : $($adapter.Description)"
+    Write-Host "MAC Address     : $($adapter.MACAddress)"
+    Write-Host "DHCP Habilitado : $($adapter.DHCPEnabled)"
+    Write-Host "IP Address      : $($adapter.IPAddress)"
+    Write-Host "Subnet Mask     : $($adapter.IPSubnet)"
+    Write-Host "Default Gateway : $($adapter.DefaultIPGateway)"
+    Write-Host "DNS Servers     : $($adapter.DNSServerSearchOrder)"
+    Write-Host "VLAN ID         : $($adapter.SettingID)" # Este campo puede no reflejar la VLAN directamente
 }
